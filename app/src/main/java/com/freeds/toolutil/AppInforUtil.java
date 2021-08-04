28880a15554d5fd3a8095b5e5e3a9bfd7448d85e
xinghl <xinghl@zfxf.com>
2021-08-04 14:14:39 +0800

    添加AppInforUtil

---
+++ app/src/main/java/com/freeds/toolutil/AppInforUtil.java
@@ -0,0 +1,51 @@
+package com.freeds.toolutil;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+
+/**
+ * Created by Freeds on 2021/6/21 0021.
+ */
+public class AppInforUtil {
+
+    /**
+     * 获取应用版本名: 8.5.9
+     */
+    public static String getAppVersionName(Context context) {
+        return getPackageInfo(context).versionName;
+    }
+
+    /**
+     * 获取应用版本号 859
+     */
+    public static int getAppVersionCode(Context context) {
+        return getPackageInfo(context).versionCode;
+    }
+
+    /**
+     * 得到应用程序的包名
+     */
+    public static String getPackageName(Context context) {
+        return getPackageInfo(context).packageName;
+    }
+
+    /**
+     * 获取应用包名
+     * @param context  context
+     * @return packageInfo
+     */
+    private static PackageInfo getPackageInfo(Context context) {
+        PackageInfo packageInfo = null;
+        try {
+            PackageManager pm = context.getPackageManager();
+            packageInfo = pm.getPackageInfo(context.getPackageName(), PackageManager.GET_CONFIGURATIONS);
+
+            return packageInfo;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return packageInfo;
+    }
+
+}

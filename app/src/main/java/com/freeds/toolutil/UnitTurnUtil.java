7fa1e78a3394bebb33bbc86c9abebf72e59f4afd
xinghl <xinghl@zfxf.com>
2021-09-15 19:51:29 +0800

    添加 UnitTurnUtil 类

---
+++ app/src/main/java/com/freeds/toolutil/UnitTurnUtil.java
@@ -0,0 +1,36 @@
+package com.freeds.toolutil;
+
+import android.content.Context;
+
+/**
+ * Created by Freeds on 2021/6/1 0001.
+ */
+public class UnitTurnUtil {
+
+    /**
+     * 根据手机分辨率 px 转成 sp
+     */
+    public static int px2sp(Context context, float pxValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (pxValue / fontScale + 0.5f);
+    }
+
+
+    /**
+     * 根据手机的分辨率从 dip 的单位 转成为 px(像素)
+     */
+    public static int dip2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
+     */
+    public static int px2dip(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+}
+
+
